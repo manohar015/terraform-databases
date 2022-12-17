6d50fd4f4f8f7c9b08d416bfc7db98713fcebbce
Manohar <manohar015@gmail.com>
2022-12-17 12:42:36 +0530

    Added my Comments

--- vars.tf
+++ vars.tf
@@ -3,3 +3,11 @@ variable "MYSQL_PORT_NUMBER" {}
 variable "MYSQL_STORAGE" {}
 variable "MYSQL_ENGINE_VERSION" {}
 variable "MYSQL_INSTANCE_CLASS" {}
+variable "DOCDB_PORT" {}
+variable "DOCDB_INSTANCE_CLASS" {}
+variable "DOCDB_INSTANCE_COUNT" {}
+
+variable "ELASTICCACHE_PORT" {}
+variable "ELASTICCACHE_NODE_COUNT" {}
+variable "ELASTICCACHE_NODE_TYPE" {}
+variable "ELASTICCACHE_ENGINE_VERSION" {}
